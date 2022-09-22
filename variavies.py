947d8af37917a27f0f57559a4203c179c27bf772
Erick Lopes <erickcslopes@gmail.com>
2022-09-22 17:10:19 -0300

    02

--- variavies.py
+++ variavies.py
@@ -6,3 +6,11 @@ class correia_plana():
         self.w = largura
 
 
+massa_especifica = 0 #kg/m3
+secao_transversal = 0 #mm2
+densidade_linear = 0 #Kg/m
+diametro = 0 #cm
+rpm = 0
+angulo_contato = 0 #φ
+coeficiente_atrito = 0 #μ
+forca_max_tracao = 0 #N
